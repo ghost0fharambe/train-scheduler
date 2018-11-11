6c7cd358cd01abd811ce58f36716e104bf777edc
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2018-11-10 22:48:56 -0700

    added html and initialized firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBe_k-YhOHxT6Hjckds07PFlcSYMlTi_Fw",
+    authDomain: "train-scheduler-edff0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-edff0.firebaseio.com",
+    projectId: "train-scheduler-edff0",
+    storageBucket: "train-scheduler-edff0.appspot.com",
+    messagingSenderId: "1089930358379"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
